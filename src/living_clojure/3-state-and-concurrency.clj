a0be9ed2730effad896ddbdee7146e17b4b92a82
Morten Brudvik <morten@brudvik.com>
2021-07-18 17:52:13 +0200

    State and Concurrency

--- src/living_clojure/3-state-and-concurrency.clj
+++ src/living_clojure/3-state-and-concurrency.clj
@@ -3,3 +3,48 @@
 ; #################################
 ; Chapter 3 - State and Concurrency
 ; #################################
+
+(def who-atom (atom :Earth))
+who-atom
+@who-atom ; value of atom
+
+(reset! who-atom (atom :Mars)) ; Change value
+
+(def who-atom (atom :Earth))
+(defn change [state]
+  (case state
+    :Earth :Mars
+    :Mars  :Jupiter
+    :Jupiter))
+(swap! who-atom change) ; swap value 
+@who-atom
+(swap! who-atom change)
+
+(def counter (atom 0))
+(dotimes [_ 4] (swap! counter inc))
+@counter
+
+(def counter (atom 0))
+(let [n 5]
+  (future (dotimes [_ n] (swap! counter inc))) 
+  (future (dotimes [_ n] (swap! counter inc)))
+  (future (dotimes [_ n] (swap! counter inc))))
+@counter
+
+
+(defn inc-print [val]
+  (println val)
+  (inc val))
+(swap! counter inc-print)
+
+(def counter (atom 0))
+(let [n 2]
+  (future (dotimes [_ n] (swap! counter inc-print)))
+  (future (dotimes [_ n] (swap! counter inc-print)))
+  (future (dotimes [_ n] (swap! counter inc-print))))
+@counter
+
+
+; ##################################
+; Using Refs for Coordinated Changes
+; ##################################
